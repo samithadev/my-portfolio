f70b94e93e4ea7630de93a7335d6f380f8935353
Samitha Wickramasinghe <89331663+samithadev@users.noreply.github.com>
2024-01-12 12:19:37 +0530

    feat: add styles and responsive to the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,49 @@
-import React from "react";
-import wave from '../assets/wave-img.png'
+import React, { useState } from "react";
+import wave from "../assets/wave-img.png";
+import { FiAlignRight } from "react-icons/fi";
+import { FiX } from "react-icons/fi";
 
 function Navbar() {
-  return(
-    <nav className="flex flex-row justify-between bg-orange-300 h-20 items-center px-8">
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className=" flex-wrap shadow-lg">
+      <div className="flex flex-row justify-between items-center h-20 px-8">
         <div className="logo flex flex-row gap-2">
-            <img src={wave} alt="logo image" className="w-6"/>
-            <h1 className=" font-medium">Samitha</h1>
+          <img src={wave} alt="logo image" className="w-6" />
+          <h1 className=" font-medium">Samitha</h1>
         </div>
 
-        <div className="nav-links absolute bg-gray-800">
-            <ul className="">
-                <li>Home</li>
-                <li>Projects</li>
-                <li>Contact Me</li>
+        <div className="nav-links hidden md:block">
+          <ul className="flex flex-row justify-between gap-8">
+            <li>Home</li>
+            <li>Projects</li>
+            <li>Contact Me</li>
+          </ul>
+        </div>
+
+        <div className="md:hidden">
+          <button onClick={toggleNavbar}>
+            {isOpen ? <FiX size={30}/> : <FiAlignRight size={30}/>}
+          </button>
+        </div>
+      </div>
+
+      <div className=" text-center md:hidden ">
+        {isOpen && (
+            <ul className="flex flex-col gap-5 py-5">
+              <a href=""><li>Home</li></a>
+              <a href=""><li>Projects</li></a>
+              <a href=""><li>Contact Me</li></a>
             </ul>
-        </div>
-
+        )}
+      </div>
     </nav>
-  )
+  );
 }
 
 export default Navbar;
