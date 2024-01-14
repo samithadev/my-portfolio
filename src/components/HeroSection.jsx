8e135cfd4e395ab26fd543dbab2d4e7e2912779d
Samitha Wickramasinghe <89331663+samithadev@users.noreply.github.com>
2024-01-14 12:16:31 +0530

    feat: add responsiveness to the navbar and hero section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -6,12 +6,12 @@ import { FaDribbble } from "react-icons/fa";
 
 function HeroSection() {
   return (
-    <div className=" mx-9 my-6">
-      <div className="hero-image w-full h-80  rounded-3xl overflow-hidden bg-white">
+    <div className=" mx-9 my-6 flex flex-col md:flex-row md:gap-5 justify-center">
+      <div className="hero-image w-full h-80 md:w-[400px] md:h-[400px]  rounded-3xl overflow-hidden bg-red-500 mt-6">
         <img src={profileimg} alt="profile img" />
       </div>
 
-      <div className="hero-details mt-6 rounded-3xl p-8 bg-gradient-to-r from-yellow-100  to-yellow-50 background-animate backdrop-blur-2xl">
+      <div className="hero-details mt-6 md:w-[400px] md:h-[400px] rounded-3xl p-8 bg-gradient-to-r from-yellow-100  to-yellow-50 background-animate backdrop-blur-2xl">
         <div>
           <h1 className=" text-2xl font-medium leading-7">
             Hello, I’m Samitha, a frontend Developer specialized in React.
@@ -22,9 +22,9 @@ function HeroSection() {
           </p>
         </div>
 
-        <div className="mt-6 flex flex-col w-ful gap-4">
+        <div className="mt-6 md:mt-8 flex flex-col w-ful gap-4 md:gap-7">
           <button className=" bg-black  text-white p-3 rounded-3xl">Contact me</button>
-          <div className="flex flex-row items-center justify-center gap-8">
+          <div className="flex flex-row items-center justify-center gap-8 ">
             <button className="bg-white p-3 rounded-full">
               <FaLinkedin/>
             </button>
