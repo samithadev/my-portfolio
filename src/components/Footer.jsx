045cf3dddd70ce7c830709154bd43ad74ab7dacd
Samitha Wickramasinghe <89331663+samithadev@users.noreply.github.com>
2024-02-03 21:34:23 +0530

    add:change footer bg color

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,20 +4,22 @@ import { FiArrowRightCircle } from "react-icons/fi";
 function Footer() {
   return (
     <div id="footer" className="flex justify-center h-[300px] mx-9 my-6">
-      <div className="h-[300px] w-full md:w-[800px] rounded-xl shadow-lg bg-gradient-to-r from-yellow-100  to-yellow-50 background-animate backdrop-blur-2xl p-8">
+      <div className="h-[300px] w-full md:w-[800px] rounded-xl shadow-lg bg-gradient-to-t from-indigo-200 via-purple-200 to-pink-200 background-animate backdrop-blur-3xl p-8">
         <h1 className="text-2xl sm:text-5xl ">Want to work together?</h1>
-        
+
         <p className=" mt-5">
-        Feel free to reach out for collaborations or just a friendly hello.
+          Feel free to reach out for collaborations or just a friendly hello.
         </p>
 
         <div className="mt-2 sm:mt-16">
-          <a href="mailto:contact.samitha@gmail.com" className="flex flex-row items-center gap-2 ">
+          <a
+            href="mailto:contact.samitha@gmail.com"
+            className="flex flex-row items-center gap-2 "
+          >
             <h1 className="text-sm sm:text-3xl">contact.samitha@gmail.com</h1>
-            <FiArrowRightCircle size={20}/>
+            <FiArrowRightCircle size={20} />
           </a>
         </div>
-
       </div>
     </div>
   );
